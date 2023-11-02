f85803103a639a41d33418cef352a7e4b60904e3
JJOptimist <optimistkum888@gmail.com>
2023-11-02 15:56:24 +0100

    eth address test

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,10 +1,41 @@
 import type { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text, heading} from '@metamask/snaps-ui';
+import { panel, text, heading } from '@metamask/snaps-ui';
+
+/**
+ * Function to retrieve and display the Ethereum account address.
+ *
+ * @returns The Ethereum account address.
+ */
+async function displayEthereumAddress() {
+  try {
+    // Request Ethereum account access from Metamask
+    await ethereum.request({ method: 'eth_requestAccounts' });
+
+    // Get the Ethereum account address
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    const ethereumAddress = accounts[0];
+
+    // Display the Ethereum address using Snaps UI components
+    return snap.request({
+      method: 'snap_dialog',
+      params: {
+        type: 'confirmation',
+        content: panel([
+          heading('Ponte Snap'),
+          text(`Hello, **${origin}**!`),
+          text(`This is your Ethereum address: ${ethereumAddress}`),
+        ]),
+      },
+    });
+  } catch (error) {
+    throw new Error('Failed to retrieve Ethereum address');
+  }
+}
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
  *
- * @param args - The request handler args as object.
+ * @param args - The request handler args as an object.
  * @param args.origin - The origin of the request, e.g., the website that
  * invoked the snap.
  * @param args.request - A validated JSON-RPC request object.
@@ -22,13 +53,8 @@ export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
             heading('Ponte Snap'),
             text(`Hello, **${origin}**!`),
             text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
           ]),
         },
       });
-    default:
-      throw new Error('Method not found.');
   }
 };
