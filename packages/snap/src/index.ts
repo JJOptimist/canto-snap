1010f9fecc66821a5b50846e42ab7a95dd410e6a
JJOptimist <optimistkum888@gmail.com>
2023-11-03 13:42:00 +0100

    removed await ethereum

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -6,31 +6,7 @@ import { panel, text, heading } from '@metamask/snaps-ui';
  *
  * @returns The Ethereum account address.
  */
-async function displayEthereumAddress() {
-  try {
-    // Request Ethereum account access from Metamask
-    await ethereum.request({ method: 'eth_requestAccounts' });
 
-    // Get the Ethereum account address
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
-    const ethereumAddress = accounts[0];
-
-    // Display the Ethereum address using Snaps UI components
-    return snap.request({
-      method: 'snap_dialog',
-      params: {
-        type: 'confirmation',
-        content: panel([
-          heading('Ponte Snap'),
-          text(`Hello, **${origin}**!`),
-          text(`This is your Ethereum address: ${ethereumAddress}`),
-        ]),
-      },
-    });
-  } catch (error) {
-    throw new Error('Failed to retrieve Ethereum address');
-  }
-}
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
