7186b85e519017f6df700d8ccd4058274794e2bc
JJOptimist <86833563+JJOptimist@users.noreply.github.com>
2023-11-05 22:53:03 +0100

    front rework 1.0

--- packages/site/src/components/Header.tsx
+++ packages/site/src/components/Header.tsx
@@ -64,7 +64,7 @@ export const Header = ({
     <HeaderWrapper>
       <LogoWrapper>
         <SnapLogo color={theme.colors.icon?.default} size={36} />
-        <Title>template-snap</Title>
+        <Title>Canto Snap</Title>
       </LogoWrapper>
       <RightContainer>
         <Toggle
